83cfe1500fc5cf9684e78103dac06db5ae2031dc
Samuel Cochran <sj26@sj26.com>
2013-10-09 12:51:48 +1100

    A more useful set of gems, all ready to go

--- config/application.rb
+++ config/application.rb
@@ -1,28 +1,24 @@
 require File.expand_path('../boot', __FILE__)
 
-# Pick the frameworks you want:
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
 module Sembl
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = "Melbourne"
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.generators do |g|
+      g.hidden_namespaces += [:test_unit, :erb]
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+      g.template_engine :slim
+      g.javascript_engine :coffee
+      g.stylesheet_engine :sass
+      g.test_framework :rspec
+      g.fixture_replacement :factory_girl
+    end
   end
 end
