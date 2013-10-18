6fcfe0841aafbb32534d0a1fc01a7a3b093c92c1
Narinda Reeders <narindajane@gmail.com>
2013-10-18 16:44:12 +1100

    Add scopes to games for game progress, available open games, games you host and games you are participating in.

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,5 +5,36 @@ class Game < ActiveRecord::Base
   belongs_to :creator, class_name: "User"
   belongs_to :updator, class_name: "User"
 
-  has_many :users, through: :player 
+  has_many :users, through: :players
+  has_many :players
+
+
+  # Games still open to users to join
+  def self.open_to_join
+    where(invite_only: false).
+      joins(:users, :board).
+      group("games.id, boards.number_of_players").
+      having("count(users.id) < boards.number_of_players")
+  end
+
+  def self.completed
+    where(state: 'completed')
+  end
+
+  def self.in_progress
+    where("state != 'draft' AND state != 'completed'")
+  end
+
+  def self.invite_only
+    where(invite_only: true)
+  end
+
+  def self.participating(current_user)
+    joins(:users).where(["users.id = ?", current_user.id])
+  end
+
+  def self.hosted_by(current_user)
+    where(creator: current_user)
+  end
+
 end
