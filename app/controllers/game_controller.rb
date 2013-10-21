8f19746ff9f72946c45f374c3b711117bf13a623
Narinda Reeders <narindajane@gmail.com>
2013-10-22 10:48:56 +1100

    Update game controller to use decent exposure

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,28 +1,13 @@
 class GameController < ApplicationController
 
-  def index
-    @open_games = Game.in_progress.open
-    @current_games = Game.in_progress.participating(current_user)
+  expose(:open_games) { Game.in_progress.open }
+  expose(:current_games) { Game.in_progress.participating(current_user) }
+  expose(:completed_games) { Game.completed.participating(current_user) }
+  expose(:hosted_games) { Game.hosted_by(current_user) }
 
-    @completed_games = Game.completed.participating(current_user)
-    @hosted_games = Game.hosted_by(current_user)
-  end
-
-  def create
-
-  end
-
-  def new
-
-  end
 
   def summary
 
   end
-
-  def show
-
-  end
-
 
 end
