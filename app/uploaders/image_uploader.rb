69c05196c4e2a6270140c7415145d2cbbe909264
Samuel Cochran <sj26@sj26.com>
2013-12-04 14:01:07 +1100

    Use local upload storage in development

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,13 +4,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MimeTypes
   include CarrierWave::MiniMagick
 
-  storage :fog
-
   process :set_content_type
-
-  def store_dir
-    "uploads/#{model.class.model_name.collection}/#{model.id}/#{mounted_as}"
-  end
 
   def extension_white_list
     %w(jpg jpeg gif png)
@@ -20,7 +14,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     process resize_to_fill: [150, 150]
   end
 
-  version(:browse_thumb) do 
+  version(:browse_thumb) do
     process resize_to_fit: [100000, 350]
   end
 
@@ -28,12 +22,8 @@ class ImageUploader < CarrierWave::Uploader::Base
     @name ||= "#{secure_token}.#{file.extension.downcase}" if original_filename
   end
 
-  def store_dir
-    "images/#{model.id}"
-  end
 
-
-  private
+private
 
   def secure_token
     ivar = "@#{mounted_as}_secure_token"
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,18 +1,27 @@
 CarrierWave.configure do |config|
-  # S3 + CloudFront settings
-  config.fog_credentials = {
-    provider: "AWS",
-    aws_access_key_id: ENV["UPLOADER_S3_ACCESS_KEY_ID"],
-    aws_secret_access_key: ENV["UPLOADER_S3_SECRET_ACCESS_KEY"],
-    region: ENV["UPLOADER_S3_REGION"]
-  }
-  config.fog_directory  = ENV["UPLOADER_S3_BUCKET"]
-  config.fog_public = true
-  config.fog_attributes = {
-    "Cache-Control" => "max-age=4838400, public",
-    "Expires" => 10.years.from_now.httpdate
-  }
-  if ENV["CLOUD_FRONT_DOMAIN"].present?
-    config.asset_host = "https://#{ENV['CLOUD_FRONT_DOMAIN']}"
+  if Rails.env.development?
+    config.storage = :file
+    config.store_dir = lambda { |uploader| "uploads/#{uploader.model.class.model_name.collection}/#{uploader.model.id}/#{uploader.mounted_as}" }
+  else
+    config.storage = :fog
+    config.store_dir = lambda { |uploader| "#{uploader.model.class.model_name.collection}/#{uploader.model.id}/#{uploader.mounted_as}" }
+
+    # S3 + CloudFront settings
+    config.fog_credentials = {
+      provider: "AWS",
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region: ENV["AWS_REGION"]
+    }
+    config.fog_directory  = ENV["AWS_S3_BUCKET"]
+    config.fog_public = true
+    config.fog_attributes = {
+      "Cache-Control" => "max-age=4838400, public",
+      "Expires" => 10.years.from_now.httpdate
+    }
+
+    if ENV["CLOUDFRONT_DOMAIN"].present?
+      config.asset_host = "https://#{ENV['CLOUDFRONT_DOMAIN']}"
+    end
   end
 end
