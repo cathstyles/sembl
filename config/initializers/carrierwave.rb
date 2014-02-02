d190d0923cf5706617855a257ac92f9fe285d5cb
Hugh Evans <hugh@artpop.com.au>
2014-02-03 10:28:45 +1100

    Share the same Fog ENV vars as AssetSync

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,9 +7,9 @@ CarrierWave.configure do |config|
     provider: "AWS",
     aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-    region: ENV["AWS_REGION"]
+    region: ENV["FOG_REGION"]
   }
-  config.fog_directory  = ENV["AWS_S3_BUCKET"]
+  config.fog_directory  = ENV["FOG_DIRECTORY"]
   config.fog_public = true
   config.fog_attributes = {
     "Cache-Control" => "max-age=4838400, public",
