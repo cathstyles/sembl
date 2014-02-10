6a7ccba788a654b9ef91c4297fccb5255f74e512
Tim Sell <tim@songkick.com>
2014-02-10 18:10:15 +1100

    cleanup parameters

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,13 +2,21 @@ class SearchController < ApplicationController
   respond_to :html, :json
 
   def index
-    @things = Services.search_service.search(Thing, Search::ThingQuery.new(search_params))
+    if search_params[:text]
+      @things = Services.search_service.search(Thing, Search::ThingQuery.new(search_params))
+    else
+      @things = []
+    end
     respond_with @things
   end
 
   private
 
   def search_params
-    params.permit(:text, :place_filter, :access_filter, :date_from, :date_to, :created_to, :game_seed)
+    @search_params ||= params.permit(
+      :text, :place_filter, :access_filter, :date_from, :date_to, :created_to, :game_seed
+    ).delete_if do |k,v|
+      v.strip.empty?
+    end
   end
 end
