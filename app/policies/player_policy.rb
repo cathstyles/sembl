0d7cb2ac585719c7466ab055363827143012a02c
Tim Sell <trsell@gmail.com>
2014-04-17 16:28:12 +1000

    player policy

--- app/policies/player_policy.rb
+++ app/policies/player_policy.rb
@@ -1,4 +1,15 @@
 class PlayerPolicy < ApplicationPolicy
 
-  
+  def create?
+    !!user && (user.id == record.game.creator_id || user.admin?) && record.game.draft?
+  end
+
+  def destroy?
+    puts 'destroy'
+    puts 'user', user.inspect
+    puts 'game', record.game.inspect
+    puts 'admin', user.admin?
+    puts 'draft', record.game.draft?
+    !!user && (user.id == record.game.creator_id || user.admin?) && record.draft?
+  end
 end
