81a691b92c65fa1be9fcde00dbb088c0b832b30c
Tim Sell <trsell@gmail.com>
2014-04-22 14:25:22 +1000

    set elasticsearch url to bonsai url if available

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,4 +1,8 @@
+if ENV['BONSAI_URL']
+  ENV['ELASTICSEARCH_URL'] = ENV['BONSAI_URL']
+end
+
 Rails.application.config.elasticsearch = {
   log: true,
-  host: ENV['ELASTICSEARCH_HOST']
+  host: ENV['ELASTICSEARCH_URL']
 }
