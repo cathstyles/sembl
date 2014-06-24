c547b86ddeea64dbb79f74e4a9d0a58515fb2f82
Nick Binnington <nick.binnington@gmail.com>
2014-06-25 09:53:08 +1000

    After sign in, redirect user to their initially requested page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,23 @@ class ApplicationController < ActionController::Base
   # decent_configuration do
   #   strategy DecentExposure::StrongParametersStrategy
   # end
-  
+  before_filter :store_location
+
+  def store_location
+    # store last url - this is needed for post-login redirect to whatever the user last visited.
+    if (request.fullpath != "/users/sign_in" &&
+        request.fullpath != "/users/sign_up" &&
+        request.fullpath != "/users/password" &&
+        request.fullpath != "/users/sign_out" &&
+        !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
+  end
+
   private
 
     def user_not_authorized
