9f9d2c1e0a277bfea805d0788e2771ba3a6c490c
Nick Binnington <nick.binnington@gmail.com>
2014-06-25 10:01:33 +1000

    Add sign up link to flash message and make it a 'notice' flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,7 +34,7 @@ class ApplicationController < ActionController::Base
         flash[:error] = "You are not authorized to perform this action."
         redirect_to request.headers["Referer"] || root_path
       else
-        flash[:error] = "Please sign in or sign up"
+        flash[:error] = "Please sign in below<br/>or sign up #{ActionController::Base.helpers.link_to "here", new_user_registration_path}.".html_safe
         redirect_to new_user_session_path
       end
     end
