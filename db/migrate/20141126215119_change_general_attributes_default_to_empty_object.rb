81786b6a104e9914905ed773df6005330ec7473d
Tim Riley <tim@openmonkey.com>
2014-11-30 12:58:17 +1100

    Fix the things general_attributes migration
    It doesn’t conflict with the implementation of `Thing.filter_keys` now.

--- db/migrate/20141126215119_change_general_attributes_default_to_empty_object.rb
+++ db/migrate/20141126215119_change_general_attributes_default_to_empty_object.rb
@@ -1,8 +1,16 @@
+# Use a naked class so that we don't get errors when `Thing.filter_keys` is
+# run inside of its `searchable` block. Before this migration is run,
+# `.filter_keys` will return errors, since it depends on all things having
+# `{}` as the defalut value for general_attributes.
+class BareThing < ActiveRecord::Base
+  self.table_name = "things"
+end
+
 class ChangeGeneralAttributesDefaultToEmptyObject < ActiveRecord::Migration
   def up
     # Convert all the empty array attributes (the previous default) to empty objects (the new default)
-    Sunspot.session = Sunspot::SessionProxy::SilentFailSessionProxy.new #(Sunspot.session)
-    Thing.find_each do |thing|
+    Sunspot.session = Sunspot::SessionProxy::SilentFailSessionProxy.new
+    BareThing.find_each do |thing|
       if thing.general_attributes.blank?
         thing.update_attribute :general_attributes, {}
       end
